3ac1e1e9a1e109c951a399fa6f342143a77812bd
Sam Maurer <maurer@berkeley.edu>
2018-06-25 08:41:38 -0700

    Initial work on unit tests

---
+++ urbansim_templates/tests/test_modelmanager.py
@@ -0,0 +1,18 @@
+from __future__ import print_function
+
+import numpy as np
+import os
+import pandas as pd
+import pytest
+
+import orca
+
+print(os.getcwd())
+
+from urbansim_templates import modelmanager as mm
+
+def test1():
+    assert 1 == 1
+    
+def test2():
+    assert a == 2
